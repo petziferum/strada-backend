799d4f3634ea88dc5399c1727cb3a0e2135bf8cd
unknown <dlewis200220@gmail.com>
2020-07-27 10:13:07 +0200

    +repaired stradamodel +singleitem

---
+++ src/main/java/com/test/one/model/InspectionSecurityPackage/SecurityFlagPackage/SingleItems/SingleItem.java
@@ -0,0 +1,44 @@
+package com.test.one.model.InspectionSecurityPackage.SecurityFlagPackage;
+
+import org.springframework.context.annotation.Description;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class SingleItem {
+    @Id
+    String Id;
+    String name;
+    String description;
+    String value;
+
+    public SingleItem(String name, String description, String value) {
+        this.name = name;
+        this.description = description;
+        this.value = value;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
